b28532cfc8ef9e81f2fce08fdde7871dd79f69f4
Kamago <kresnaimp11@gmail.com>
2020-10-19 09:37:00 +0700

    method password verify

--- application/libraries/Password.php
+++ application/libraries/Password.php
@@ -3,4 +3,12 @@
         public function hash($password){
             return password_hash($password, PASSWORD_BCRYPT);
         }
+
+        public function verify($password, $hash); //$hash untuk mengambil nilai password pada db
+            if(password_verify($password, $hash)){
+                return true;
+            }
+
+            return false;
+        
     }
